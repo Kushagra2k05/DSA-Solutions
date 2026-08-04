6daea4b0d66dafa8e3eeded5fdee45918de56c5b
Kushagra2k05 <kushagrasrivastava0503@gmail.com>
2026-08-04 20:18:56 +0530

    Add Riptide [A] from Codeforces

---
+++ 2254/A - Riptide/solution.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+ 
+        int rounds = 0;
+ 
+        while (a != b && b != c && a != c) {
+ 
+            if (a > b && a > c) {          
+                a--;
+                if (b < c)
+                    b++;
+                else
+                    c++;
+            }
+            else if (b > a && b > c) {     
+                b--;
+                if (a < c)
+                    a++;
+                else
+                    c++;
+            }
+            else {                         
+                c--;
+                if (a < b)
+                    a++;
+                else
+                    b++;
+            }
+ 
+            rounds++;
+        }
+ 
+        cout << rounds << "
+";
+    }
+ 
+    return 0;
+}
